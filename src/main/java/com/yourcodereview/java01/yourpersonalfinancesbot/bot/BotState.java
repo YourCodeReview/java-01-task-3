8197302b9a2863f06f2d400444f2d589ca25e099
Dennis Yudin <denum13@yandex.ru>
2021-12-26 12:42:39 +0300

    add buttons

---
+++ src/main/java/com/yourcodereview/java01/yourpersonalfinancesbot/bot/BotState.java
@@ -0,0 +1,8 @@
+package com.yourcodereview.java01.yourpersonalfinancesbot.bot;
+
+public enum BotState {
+    SHOW_MAIN_MENU,
+    ADD_EXPENSE,
+    ADD_INCOME
+}
+
---
+++ src/main/java/com/yourcodereview/java01/yourpersonalfinancesbot/bot/InputMessageHandler.java
@@ -0,0 +1,11 @@
+package com.yourcodereview.java01.yourpersonalfinancesbot.bot;
+
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+
+public interface InputMessageHandler {
+
+    SendMessage handle(Message message);
+
+    BotState getHandlerName();
+}
